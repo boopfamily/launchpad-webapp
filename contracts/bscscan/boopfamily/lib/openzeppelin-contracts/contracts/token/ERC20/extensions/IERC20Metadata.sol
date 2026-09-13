1f7a5ca698c96052fe4683d4bbd37ad4bb9d1393
boopfamily <smartkolsdev4@gmail.com>
2026-09-14 06:09:01 +0700

    Add clean contract source: lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol

---
+++ contracts/bscscan/boopfamily/lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.4.0) (token/ERC20/extensions/IERC20Metadata.sol)
+
+pragma solidity >=0.6.2;
+
+import {IERC20} from "../IERC20.sol";
+
+interface IERC20Metadata is IERC20 {
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function decimals() external view returns (uint8);
+}
